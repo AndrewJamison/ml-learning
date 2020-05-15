789f699005eb8251ff7f3f1b8d30ae9d88e359a7
Andrew Jamison <andrew.jamison27@gmail.com>
2020-05-15 16:35:57 -0600

    loading data into 2d vector

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
-#include <iostream>;
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
-using namespace std;
 
-int Main() {
 
+int main() {
+    std::ifstream infile;
+    infile.open("data.csv");
+    std::string line, item;
+    
+    std::vector<std::vector<int>> data(2);
+    std::vector<int> row(2);
+    if(infile.is_open()){
+        while (getline(infile,line)){
+            if (not!infile.good()) break;
+            std::stringstream strs(line);
+            while(getline(strs, item, ',')) row.push_back(std::stoi(item));
+            data.push_back(row);
+        };
+    };
     return 0;
-}
-    
-    
-    
+};
