f43d74d6895f47e5f3685de950de3b1cf42f6119
Andrew Jamison <andrew.jamison27@gmail.com>
2020-05-15 20:39:52 -0600

    Finally loaded data into a vector properly

--- main.cpp
+++ main.cpp
@@ -4,21 +4,42 @@
 #include <sstream>
 
 
+class linearReg {
+    public:
+     void printData(std::vector< std::vector<int> > data){
+        for(int x = 0; x < data.size(); x++) {
+            for (int y = 0; y < data[0].size(); y++) {
+                std::cout << data[x][y] << " ";
+            }
+            std::cout << "\n";
+        };
+    }
+};
+
+
 
 int main() {
     std::ifstream infile;
-    infile.open("data.csv");
     std::string line, item;
-    
-    std::vector<std::vector<int>> data(2);
-    std::vector<int> row(2);
+    std::vector< std::vector<int> > data;
+    std::vector<int> row;
+    int num;
+    linearReg lr;
+    char *ptr = NULL;
+    infile.open("data.csv");
     if(infile.is_open()){
-        while (getline(infile,line)){
-            if (not!infile.good()) break;
+        std::cout << "File Open \n";
+        while (std::getline(infile,line)){
+            if (!infile.good()) break;
             std::stringstream strs(line);
-            while(getline(strs, item, ',')) row.push_back(std::stoi(item));
+            while(std::getline(strs, item, '|')){
+                num = std::strtol(item.c_str(),&ptr, 10);
+                row.push_back(num);
+            };
             data.push_back(row);
+            row.clear();
         };
     };
+    lr.printData(data);
     return 0;
 };
